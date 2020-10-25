75eca89633d0879dfdcdfdacb3142a29084d8f1b
andvetto <andvetto86@gmail.com>
2020-10-25 12:16:40 +0100

    'update'

--- lib/config-remoto.php
+++
@@ -1,9 +0,0 @@
-<?php
-			//remoto
-			
-	define('DB_HOST','fdb26.awardspace.net');
-	define('DB_NAME','3239491_filminfo');
-	define('DB_USER','3239491_filminfo');
-	define('DB_PASSWORD','K@paro86');
-	
-	
